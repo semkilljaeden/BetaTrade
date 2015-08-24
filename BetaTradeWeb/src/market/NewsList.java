73367b0bd009ddeda1378016e8a5c6cb67ecffdf
Kil'jaeden <killjaeden@gmail.com>
2015-08-24 13:18:59 +0800

    aaaa

--- BetaTradeWeb/src/market/NewsList.java
+++ BetaTradeWeb/src/market/NewsList.java
@@ -1,11 +1,20 @@
 package market;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import entity.InstrumentHistory;
 
 @XmlRootElement(name="SecurityHeadlines")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class NewsList {
+    @XmlElementWrapper(name="Headlines")
+    @XmlElement(name="SecurityHeadline")
+    private List<HeadLines> news;
 
 }
